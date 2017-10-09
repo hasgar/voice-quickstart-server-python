c60a7df59dd76d88093e44c0cc8fb02136d0b74b
Hasgar D <hasgardee@gmail.com>
2017-10-09 11:10:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -46,8 +46,8 @@ def outgoing():
       .create(to="+919020708979",  # Any phone number
               from_="+12517322701 ", # Must be a valid Twilio number
   url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
-  resp.say("Your call is connecting.please wait")
   return str(resp)
+
 @app.route('/incoming', methods=['GET', 'POST'])
 def incoming():
   resp = twilio.twiml.Response()
